d67c06bcbf7bd40544a9790b03e802c9e03f332d
PyeongGang_Kim <pyeonggangkim@gmail.com>
2019-07-09 17:32:05 +0900

    g

---
+++ 00_startCamp/02_Day/02_file_control.py
@@ -0,0 +1,6 @@
+import os
+os.chdir(r'C:\Users\student\TIL\00_startCamp\02_Day\dummy')
+
+for filename in os.listdir('.'):
+    # os.rename(filename,f'SAMSUNG_{filename}')
+    os.rename(filename,filename.replace('SAMSUNG_','SSAFY_'))
