6b66d6862bca74c27ff04a209c690d46bc2f6703
PyeongGang-Kim <pyeonggangkim@gmail.com>
2019-07-21 23:20:31 +0900

    7/21

--- SW_Expert_Arcademy/3-09.py
+++ SW_Expert_Arcademy/3-09.py
@@ -1,7 +1,13 @@
+import sys
+sys.stdin=open('input.txt', 'r')
+
 T=int(input())
 for t in range(1,T+1):
+    
+    #문제입력 양쪽에서 공백이 있는 경우가 있기 때문에 strip()을 추가하였음.
     N, M = map(int, input().split(" "))
     ai=list(map(int, input().split(" ")))
+
     result=0
     maxnum, minnum = sum(ai[0:M]), sum(ai[0:M])
 
