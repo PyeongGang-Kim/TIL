8aab99e1ccc1441706e1d16daa6db91c7ab07810
PyeongGang_Kim <pyeonggangkim@gmail.com>
2019-07-23 18:01:30 +0900

    7/23

---
+++ 01_Python/notes/myPackage/math/formula.py
@@ -0,0 +1,8 @@
+pi=3.14
+def my_max(a,b):
+    if a>b:
+        print('{}가 {}보다 큽니다.'.format(a,b))
+    elif a==b:
+        print('{}와 {}가 같습니다.'.format(a,b))
+    else:
+        print('{}가 {}보다 큽니다.'.format(b,a))
