6ce69601a4e51f7da74266cbd84ac969f7e55d5b
PyeongGang_Kim <pyeonggangkim@gmail.com>
2019-08-19 18:01:50 +0900

    8/19

--- algorithm/work/test.py
+++ algorithm/work/test.py
@@ -1,5 +1,20 @@
-T = int(input())
-for t in range(1, T+1):
-    N = int(input())
-    sum_num = (N//2+N%2)*(-1)**(N%2+1)
-    print('#{} {}'.format(t, sum_num))
+def Bbit_print(a):
+    for i in range(7, -1, -1):
+        if a & (1<<i):
+            print(1, end='')
+        else:
+            print(0, end='')
+    print()
+
+a= 0x86
+key = 0xAA
+print('a      ==>', end = ' ')
+Bbit_print(a)
+
+print('a^=key ==>', end = ' ')
+a ^= key
+Bbit_print(a)
+
+print('a^=key ==>', end = ' ')
+a ^= key
+Bbit_print(a)
--- algorithm/work/test2.py
+++ algorithm/work/test2.py
@@ -1,46 +1,20 @@
-def nextp():
-    global nd, B, p
-    if goP(p):
-        p = [p[0]+d[nd][0], p[1]+d[nd][1]]
-        C[p[1]][p[0]] = B.pop(0)
-    else:
-        nd = (nd + 1)%4
-        if goP(p):
-            p = [p[0]+d[nd][0], p[1]+d[nd][1]]
-            C[p[1]][p[0]] = B.pop(0)
+T = 10
+for t in range(1, T+1):
+    nnnnn = input()
+    nl = list(map(int, input().split()))
+    nnnn = int(input())
+    ol = input().split()
+    for k in range(nnnn):
+        ol.pop(0)
+        i = int(ol.pop(0))
+        n = int(ol.pop(0))
+        if i < 10:
+            for k in range(n):
+                nl.insert(i, ol.pop(0))
         else:
-            return
-    return
+            for k in range(n):
+                ol.pop(0)
+    print('#{}'.format(t), end='')
+    for i in range(10):
+        print(' {}'.format(nl.pop(0)), end='')
 
-
-def goP(p):
-    global nd
-    tmpp = [p[0] + d[nd][0], p[1] + d[nd][1]]
-    if 0<=tmpp[0]<al and 0<=tmpp[1]<al:
-        if C[tmpp[1]][tmpp[0]]:
-            return False
-        else:
-            return True
-    return False
-
-
-A = [[9,20,2,18,11],[19,1,25,3,21],[8,24,10,17,7],[15,4,16,5,6],[12,13,22,23,14]]
-al = len(A)
-print(A)
-B=[]
-C = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
-for a in A:
-    B=B+a
-for i in range(len(B)):
-    for j in range(len(B)-1):
-        if B[j]>B[j+1]:
-            B[j], B[j+1] = B[j+1], B[j]
-d = [[1,0], [0,1], [-1,0],[0,-1]]
-
-nd = 0
-p = [0, 0]
-C[0][0] = B.pop(0)
-while B:
-    nextp()
-for c in C:
-    print(c)
