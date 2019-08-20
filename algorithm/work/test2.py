7fa5bd2880cd18d3fb1624d06150ad13d5004167
PyeongGang_Kim <pyeonggangkim@gmail.com>
2019-08-20 17:54:20 +0900

    8/20

--- algorithm/work/test2.py
+++ algorithm/work/test2.py
@@ -1,29 +1,15 @@
-def cf(c=0):
-    if c >= n:
-        chklg(nl)
-        return
-
-    for i in range(len(nl)-1):
-        for j in range(i+1, len(nl)):
-            nl[j], nl[i] = nl[i], nl[j]
-            cf(c+1)
-            nl[j], nl[i] = nl[i], nl[j]
-
-
-def chklg(nll):
-    global maxnum
-    tmp2 = 0
-    for i in range(len(nll)):
-        tmp2 += int(nll[i])*10**(len(nll)-i-1)
-    if tmp2 > maxnum:
-        maxnum = tmp2
-
-
 T = int(input())
 for t in range(1, T+1):
-    numb, n = input().split()
-    nl = list(numb)
-    n = int(n)
-    maxnum = int(numb)
-    cf()
-    print('#{} {}'.format(t,maxnum))
+    N, A, B = map(int, input().split())
+    n = 1
+    while n**2<N:
+        n += 1
+    result = B*(N-1)
+    for r in range(1, N):
+        for c in range(r, N//r+1):
+            tmp = A*abs(r-c)+B*(N-r*c)
+            if tmp < result:
+                result = tmp
+    print('#{} {}'.format(t,result))
+
+
