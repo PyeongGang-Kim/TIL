851ca045a0112db87b7a0ec3c825ce5497a315c4
PyeongGang_Kim <pyeonggangkim@gmail.com>
2019-08-21 19:47:27 +0900

    8/21

--- algorithm/work/test2.py
+++ algorithm/work/test2.py
@@ -2,20 +2,42 @@ import sys
 sys.stdin = open('test2_input.txt')
 
 
+def mkr(d = 0):
+    global result
+    tmp = 0
+    tmpx, tmpy = nl[0], nl[1]
+    for i in arr:
+        tmp += abs(tmpx - nl[i * 2 + 4]) + abs(tmpy - nl[i * 2 + 5])
+        tmpx, tmpy = nl[i * 2 + 4], nl[i * 2 + 5]
+    tmp += abs(tmpx - nl[2]) + abs(tmpy - nl[3])
+    if tmp > result:
+        return
+    if d == N:
+        if tmp < result:
+            result = tmp
+        return
+    for i in range(N):
+        if i not in arr:
+            arr.append(i)
+            mkr(d+1)
+            arr.pop()
+
+
 T = int(input())
 for t in range(1, T+1):
-    N, M = map(int, input().split())
-    nl = []
-    for m in range(M):
-        nl.append(list(map(int, input().split())))
-    print(nl)
-    for n in range(1, N+1):
-        st = [n]
-        while 1:
-            for v in nl:
-                if v[0] == st[-1]:
-                    st.append(v[0])
-                    break
-                elif v[1] == st[-1]:
-                    st.append(v[1])
-                    break
+    N = int(input())
+    nl = list(map(int, input().split()))
+    company = nl[0:2]
+    home = nl[2:4]
+    arr = []
+    tmp = 0
+    tmpx, tmpy = nl[0], nl[1]
+    for i in range(N):
+        tmp += abs(tmpx - nl[i * 2 + 4]) + abs(tmpy - nl[i * 2 + 5])
+        tmpx, tmpy = nl[i * 2 + 4], nl[i * 2 + 5]
+    tmp += abs(tmpx - nl[2]) + abs(tmpy - nl[3])
+    result = tmp
+    mkr()
+    print('#{} {}'.format(t, result))
+    # print(fr)
+    # print(result)
