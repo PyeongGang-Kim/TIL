6deb962682637a713b1f809f37299520f8a54b9b
PyeongGang_Kim <pyeonggangkim@gmail.com>
2019-08-23 18:04:48 +0900

    8/23

--- algorithm/work/test2.py
+++ algorithm/work/test2.py
@@ -3,39 +3,14 @@ import sys
 sys.stdin = open('asdf.txt')
 
 
-def fnf():
-    global st
-    X = len(nl)//2
-    vl = [0 for _ in range(V+1)]
-    for i in range(X):
-        vl[nl[2*i+1]] += 1
-    for i in range(1, V+1):
-        if i not in st and not vl[i] and i in nl:
-            st.append(i)
-
-
-def delf():
-    tmp = st.pop()
-    result.append(tmp)
-    tmpst = []
-    for i in range(len(nl)//2):
-        if nl[2*i] == tmp:
-            tmpst.append(i)
-    while tmpst:
-        tmp2 = tmpst.pop()
-        nl.pop(2*tmp2)
-        nl.pop(2*tmp2)
-
-T = 10
+T = int(input())
 for t in range(1, T+1):
-    V, E = map(int, input().split())
-    nl = list(map(int, input().split()))
-    st=[]
-    result = []
-    fnf()
-    delf()
-    while nl:
-        while st:
-            delf()
-        fnf()
-    print(result)
+    N, M = map(int, input().split())
+    nl = set(input().split())
+    ml = input().split()
+    cnt = 0
+    for n in nl:
+        for m in ml:
+            nl-{m}
+    result = N - len(nl)
+    print('#{} {}'.format(t, result))
