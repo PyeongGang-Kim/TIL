6cbb37694d548a2e63684bc4a4ef505b23040d28
PyeongGang-Kim <pyeonggangkim@gmail.com>
2019-08-25 23:42:21 +0900

    8/25

--- algorithm/work/test2.py
+++ algorithm/work/test2.py
@@ -7,10 +7,5 @@ T = int(input())
 for t in range(1, T+1):
     N, M = map(int, input().split())
     nl = set(input().split())
-    ml = input().split()
-    cnt = 0
-    for n in nl:
-        for m in ml:
-            nl-{m}
-    result = N - len(nl)
-    print('#{} {}'.format(t, result))
+    ml = set(input().split())
+    print('#{} {}'.format(t, len(nl&ml)))
