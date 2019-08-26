77ef484a7575c43bd6d7b92d1c82e91e621a703f
PyeongGang_Kim <pyeonggangkim@gmail.com>
2019-08-26 20:59:43 +0900

    8/26

--- algorithm/work/test.py
+++ algorithm/work/test.py
@@ -1,2 +1,32 @@
-print(ord('a')-97)
-print(ord('z')-97)
+import sys
+sys.stdin = open('asdf.txt')
+
+def C(d=0, s=''):
+    tmp = 0
+    if d == 6:
+        if chkrungin(s[0:3]) and chkrungin(s[3:]):
+            return 1
+        return 0
+    for i in range(6):
+        if not vl[i]:
+            vl[i] = 1
+            tmp = C(d+1, s+nu[i])
+            vl[i] = 0
+            if tmp:
+                break
+    return tmp
+
+
+def chkrungin(S):
+    if S[0] == S[1] and S[1] == S[2]:
+        return 1
+    if int(S[0]) == int(S[1])+1 and int(S[1]) == int(S[2])+1:
+        return 1
+
+
+T = int(input())
+for t in range(1, T+1):
+    vl = [0]*6
+    nu = input()
+    chk = 0
+    print(C())
--- algorithm/work/test2.py
+++ algorithm/work/test2.py
@@ -5,7 +5,17 @@ sys.stdin = open('asdf.txt')
 
 T = int(input())
 for t in range(1, T+1):
-    N, M = map(int, input().split())
-    nl = set(input().split())
-    ml = set(input().split())
-    print('#{} {}'.format(t, len(nl&ml)))
+    A, B = input().split()
+    a = len(A)
+    b = len(B)
+    c = [[0 for _ in range(a+1)] for _ in range(b+1)]
+    for j in range(1, b+1):
+        for i in range(1, a+1):
+            tp = c[j][i-1]
+            if B[j-1] == A[i-1]:
+                c[j][i] = c[j - 1][i - 1] + 1
+            elif tp >= c[j-1][i]:
+                c[j][i] = tp
+            else:
+                c[j][i] = c[j-1][i]
+    print('#{} {}'.format(t, c[b][a]))
