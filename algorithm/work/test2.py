783006eae2f938da237a9bae496d3c4033475254
PyeongGang_Kim <pyeonggangkim@gmail.com>
2019-09-05 19:59:40 +0900

    9/05

--- algorithm/work/test2.py
+++ algorithm/work/test2.py
@@ -1,1 +1,35 @@
-print(-1%8)
+import sys
+sys.stdin = open('asdf.txt')
+
+def chk():
+    for j in range(9):
+        tmp = set()
+        for i in range(9):
+            tmp.add(nl[j][i])
+        if len(tmp) != 9:
+            return False
+
+    for i in range(9):
+        tmp = set()
+        for j in range(9):
+            tmp.add(nl[j][i])
+        if len(tmp) != 9:
+            return False
+
+    for j in range(3):
+        for i in range(3):
+            tmp = set()
+            for k in range(3):
+                for l in range(3):
+                    tmp.add(nl[j*3+k][i*3+l])
+            if len(tmp) != 9:
+                return False
+    return True
+
+T = int(input())
+for t in range(1, T+1):
+    nl = [list(map(int, input().split())) for _ in range(9)]
+    if chk():
+        print('#{} {}'.format(t, 1))
+    else:
+        print('#{} {}'.format(t, 0))
--- algorithm/work/test3.py
+++ algorithm/work/test3.py
@@ -1,5 +1,32 @@
-a = 2
-b = 2
-s = [(2,2)]
+import sys
+sys.stdin = open('asdf.txt')
 
-print(not (-1000 <= 1001 <= 1000 and  -1000 <= 1010 <= 1000))
+
+def bfs():
+    Q = [(0, 0)]
+    while Q:
+        x, y = Q.pop(0)
+        h = tl[y][x]
+        if x == N-1 and y == N-1:
+            continue
+        for dx, dy in dr:
+            tx, ty = x+dx, y+dy
+            if 0 <= tx < N and 0 <= ty < N:
+                if tl[ty][tx] > h + nl[ty][tx]:
+                    Q.append((tx, ty))
+                    tl[ty][tx] = h + nl[ty][tx]
+
+
+dr = [[0, 1], [1, 0], [-1, 0], [0, -1]]
+T = int(input())
+for t in range(1, T+1):
+    N = int(input())
+    nl = [[int(i) for i in list(input())] for _ in range(N)]
+    tmp = 0
+    for i in range(N):
+        for j in range(N):
+            tmp += nl[j][i]
+    tl = [[tmp for _ in range(N)] for _ in range(N)]
+    tl[0][0] = 0
+    bfs()
+    print('#{} {}'.format(t, tl[N-1][N-1]))
