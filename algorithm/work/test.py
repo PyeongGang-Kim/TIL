26200328faea7a47b7362cc131ae8632f94ddf81
PyeongGang_Kim <pyeonggangkim@gmail.com>
2019-09-26 14:50:34 +0900

    9/26

--- algorithm/work/test.py
+++ algorithm/work/test.py
@@ -1,49 +1,16 @@
-import sys
-sys.stdin = open('asdf.txt')
-from collections import deque
+def comb(dep = 0):
+    if len(arr) == n:
+        print(arr)
+        return
+    if dep == len(a):
+        return
+    arr.append(a[dep])
+    comb(dep+1)
+    arr.pop()
+    comb(dep+1)
 
+arr = []
 
-def find(i, j):
-    cnt = 1
-    while 1:
-        chk = 0
-        chk1 = 0
-        for dx, dy in d:
-            tx, ty = i + dx, j + dy
-            if 0 <= tx < N and 0 <= ty < N and nl[j][i] + 1 == nl[ty][tx]:
-                i = tx
-                j = ty
-                chk = 1
-                break
-        if not chk:
-            break
-    vl[j][i] = cnt
-    while 1:
-        chk = 0
-        for dx, dy in d:
-            tx, ty = i + dx, j + dy
-            if 0 <= tx < N and 0 <= ty < N and nl[j][i] - 1 == nl[ty][tx]:
-                i = tx
-                j = ty
-                chk = 1
-                break
-        if not chk:
-            break
-        cnt += 1
-        vl[j][i] = cnt
-
-
-d = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-T = int(input())
-for t in range(1, T + 1):
-    r = 0
-    n = 0
-    N = int(input())
-    nl = [list(map(int, input().split())) for _ in range(N)]
-    vl = [[0 for _ in range(N)] for _ in range(N)]
-    for j in range(N):
-        for i in range(N):
-            if not vl[j][i]:
-                find(i, j)
-
-    print('#{} {} {}'.format(t, n, r))
+a = [1, 2, 3, 4, 5]
+n = 4
+comb()
--- algorithm/work/test2.py
+++ algorithm/work/test2.py
@@ -1,5 +1,34 @@
-import sys
-sys.stdin = open('asdf.txt')
+T = int(input())
+for tc in range(1, T+1):
+    a, b = map(int, input().split())
+    A, B = divmod(a, b)
+    res = ''
+    if B:
+        vl = [0 for i in range(b)]
+        vl2 = []
+        tmp = -1
+        while True:
+            t1, t = divmod(10*B, b)
+            if t == 0:
+                break
+            if not vl[B]:
+                vl[B] = t
+                vl2.append(str(t1))
+                B = t
 
-
-print(16%16)
+            else:
+                tmp = vl2.index(str(t1))
+                break
+        if tmp != -1:
+            r = ['.']
+            for i in range(tmp):
+                r.append(vl2[i])
+            r1 = []
+            for i in range(tmp, len(vl2)):
+                r1.append(vl2[i])
+            res = (str(A) +''.join(r) + '(' +''.join(r1)+')')
+        else:
+            res = (round(a/b, 1))
+    else:
+        res = str(A)
+    print('#{} {}'.format(tc, res))
