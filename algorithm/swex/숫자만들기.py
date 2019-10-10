2e727c7bc83b43f63ea398df1b5f971cd6c8af3e
PyeongGang-Kim <pyeonggangkim@gmail.com>
2019-10-10 18:02:05 +0900

    10/10

--- algorithm/swex/숫자만들기.py
+++ algorithm/swex/숫자만들기.py
@@ -1,7 +1,25 @@
-def solve(arr=[0, 0, 0, 0], )
+def solve(s, arr=[0, 0, 0, 0], cnt = 1):
+    if cnt == len(ol):
+        print(s)
+        return
+    for i in range(4):
+        if arr[i] <= ol[i]:
+            arr[i] += 1
+            cnt += 1
+            if i == 0:
+                solve(s+nl[cnt], arr, cnt)
+            elif i == 1:
+                solve(s-nl[cnt], arr, cnt)
+            elif i == 2:
+                solve(s*nl[cnt], arr, cnt)
+            elif i == 3:
+                solve(s//nl[cnt], arr, cnt)
+            arr[i] -= 1
+
 
 T = int(input())
 for t in range(1, T+1):
     N = int(input())
     ol = list(map(int, input().split()))
     nl = list(map(int, input().split()))
+    solve(nl[0])
