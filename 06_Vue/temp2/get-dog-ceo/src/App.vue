3374e56404986aff413e61dab6bb8f72180abae0
PyeongGang-Kim <pyeonggangkim@gmail.com>
2019-11-07 00:31:42 +0900

    11/07

---
+++ 06_Vue/temp2/get-dog-ceo/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <DogCeo></DogCeo>
+  </div>
+</template>
+
+<script>
+import DogCeo from './components/DogCeo.vue'
+
+export default {
+  components: {
+    DogCeo,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 06_Vue/temp2/get-dog-ceo/src/components/DogCeo.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <img v-for="image in images" v-bind:key="image.id" v-bind:src="image.url">
+    <button v-on:click="getDogImage">개 사진 추가</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data: function() {
+    return {
+      images: [],
+    }
+  },
+  methods: {
+    getDogImage: function() {
+      const axios = require('axios')
+      axios.get('https://dog.ceo/api/breeds/image/random')
+      .then(res => {
+        const imgUrl = res.data.message
+        this.images.push({
+          id: Date.now(),
+          url: imgUrl
+        })
+      })
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
