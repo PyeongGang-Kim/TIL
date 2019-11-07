ed9ee986860ccad4c65b58fb59bc1559d6b2fa66
PyeongGang-Kim <pyeonggangkim@gmail.com>
2019-11-07 17:46:07 +0900

    11/07

--- 06_Vue/02_vue_webpack/src/components/TodoList.vue
+++ 06_Vue/02_vue_webpack/src/components/TodoList.vue
@@ -14,43 +14,43 @@
 <script>
 export default{
   props: {
-        // 카테고리 라는 속성의 유효성을 설정할 수 있다.
-        // 유효성 벗어나도 동작은 하지만 오류가 발생함
-        category: {
-          type: String,
-          required: true,
-          validator: function(value){
-            if (value.length < 15){
-              return true
-            } else {
-              return false
-            }
-          }
+    // 카테고리 라는 속성의 유효성을 설정할 수 있다.
+    // 유효성 벗어나도 동작은 하지만 오류가 발생함
+    category: {
+      type: String,
+      required: true,
+      validator: function(value){
+        if (value.length < 15){
+          return true
+        } else {
+          return false
         }
-      },
-      data: function() {
-        return {
-          todos: [],
-          newTodo: '',
-        }
-      },
-      methods: {
-        addTodo: function() {
-          if (this.newTodo.length !== 0){
-            this.todos.push({
-              id: Date.now(),
-              content: this.newTodo,
-              completed: false
-            })
-            this.newTodo = ''
-          }
-        }
-      },
-      removeTodo: function(todoId){
-        this.todos = this.todos.filter((todo) => {
-          return todo.id !== todoId
+      }
+    }
+  },
+  data: function() {
+    return {
+      todos: [],
+      newTodo: '',
+    }
+  },
+  methods: {
+    addTodo: function() {
+      if (this.newTodo.length !== 0){
+        this.todos.push({
+          id: Date.now(),
+          content: this.newTodo,
+          completed: false
         })
-      },
+        this.newTodo = ''
+      }
+    }
+  },
+  removeTodo: function(todoId){
+    this.todos = this.todos.filter((todo) => {
+      return todo.id !== todoId
+    })
+  },
 }
 </script>
 
