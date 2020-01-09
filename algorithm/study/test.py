ae4c711d6cc28ae03409061f878ad0ea60efac38
PyeongGang-Kim <pyeonggangkim@gmail.com>
2020-01-10 00:07:01 +0900

    01/10

--- algorithm/study/test.py
+++ algorithm/study/test.py
@@ -1,33 +1,43 @@
-def dfs(i):
-    tmp = 1
-    for idx in range(1, N+1):
-        if ml[i][idx] and not vl[idx]:
-            vl[idx] = True
-            tmp += dfs(idx)
-    return tmp
+import sys
+input = sys.stdin.readline
+def seg(s, e, su, idx = 1):
+    if nl[idx] < su:
+        return 1111111
+    if s == e:
+        return s + 1
+    m = (s + e) // 2
+    idx <<= 1
+    if nl[idx] >= su:
+        return seg(s, m, su, idx)
+    else:
+        return min(seg(s, m, su, idx), seg(m+1, e, su-nl[idx], idx + 1))
 
 
-def dfs2(i):
-    tmp = 1
-    for idx in range(1, N+1):
-        if ml[idx][i] and not vl[idx]:
-            vl[idx] = True
-            tmp += dfs2(idx)
-    return tmp
+nl = [0] * 2097152
+tmp = 1048575
+r = []
+n = int(input())
+while n:
+    n -= 1
+    tmp2 = input()
+    if tmp2[0] == '1':
+        #꺼내기
+        a, b = map(int, tmp2.split())
+        #누적합이 b보다 작거나 같은 최대의 구간
+        tmp3 = seg(0, tmp, b)
+        idx = tmp + tmp3
+        while idx:
+            nl[idx] -= 1
+            idx >>= 1
+        r.append(str(tmp3))
+
+    else:
+        a, b, c = map(int, tmp2.split())
+        # b맛 c개 넣기(b인덱스에 c 더하기)
+        idx = tmp + b
+        while idx:
+            nl[idx] += c
+            idx >>= 1
 
 
-N, M = map(int, input().split())
-ml = [[False for _ in range(N+1)] for _ in range(N+1)]
-cl = [False for i in range(N+1)]
-while M:
-    M -= 1
-    a, b = map(int, input().split())
-    ml[a][b] = True
-tmp = N // 2 + 1
-cnt = 0
-for i in range(1, N+1):
-    vl = [False for _ in range(N+1)]
-    vl[i] = True
-    if dfs(i) > tmp or dfs2(i) > tmp:
-        cnt += 1
-print(cnt)
+print('\n'.join(r))
