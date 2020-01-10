0c7db0cd54f7431f84ab7c38a2c7a8a0d87cfe4d
PyeongGang-Kim <pyeonggangkim@gmail.com>
2020-01-10 23:43:30 +0900

    01/10

--- algorithm/study/t2.py
+++ algorithm/study/t2.py
@@ -1,9 +1,5 @@
-from collections import deque
-Q = deque()
-sorted(Q)
-
-a = [[0, 4], [1, 3], [2, 2], [3, 1], [4, 0]]
-
-a.sort(key=lambda x: x[1])
-
-print(a)
+i = 1
+N = 100
+while i < N:
+    print(i&(i-1), i - (i&(-i)))
+    i += 1
--- algorithm/study/test.py
+++ algorithm/study/test.py
@@ -1,43 +1,33 @@
 import sys
 input = sys.stdin.readline
-def seg(s, e, su, idx = 1):
-    if nl[idx] < su:
-        return 1111111
-    if s == e:
-        return s + 1
-    m = (s + e) // 2
-    idx <<= 1
-    if nl[idx] >= su:
-        return seg(s, m, su, idx)
-    else:
-        return min(seg(s, m, su, idx), seg(m+1, e, su-nl[idx], idx + 1))
-
-
-nl = [0] * 2097152
-tmp = 1048575
-r = []
 n = int(input())
-while n:
-    n -= 1
-    tmp2 = input()
-    if tmp2[0] == '1':
-        #꺼내기
-        a, b = map(int, tmp2.split())
-        #누적합이 b보다 작거나 같은 최대의 구간
-        tmp3 = seg(0, tmp, b)
-        idx = tmp + tmp3
-        while idx:
-            nl[idx] -= 1
-            idx >>= 1
-        r.append(str(tmp3))
-
-    else:
-        a, b, c = map(int, tmp2.split())
-        # b맛 c개 넣기(b인덱스에 c 더하기)
-        idx = tmp + b
-        while idx:
-            nl[idx] += c
-            idx >>= 1
-
-
-print('\n'.join(r))
+m = 10 ** 9 + 7
+c = [0] * 200001
+d = [0] * 200001
+r = 1
+i = 1
+x = int(input()) + 1
+t = x
+while t <= 200000:
+    c[t] += 1
+    d[t] += x
+    t += t & -t
+s = x
+while i < n:
+    x = int(input()) + 1
+    if i:
+        t = x
+        p = q = 0
+        while t:
+            p += c[t]
+            q += d[t]
+            t &= t - 1
+        r = r * (p * x - q + s - q - (i - p) * x) % m
+    t = x
+    while t <= 200000:
+        c[t] += 1
+        d[t] += x
+        t += t & -t
+    s += x
+    i += 1
+print(r)
