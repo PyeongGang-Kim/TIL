a6fdd4b2b60cf92b8d603190ec55960a447d9002
PyeongGang-Kim <pyeonggangkim@gmail.com>
2020-01-23 00:48:25 +0900

    01/23

--- algorithm/study/17135.py
+++ algorithm/study/17135.py
@@ -1,105 +1,59 @@
-import sys
 from collections import deque
-'''
-%%
-문제를 잘못읽었음.
-문제를 먼저 이해부터 하고 들어가자
-
-
-각 적들에 인덱싱을 하고
-각 궁수를 배치 할 수 있는 부분들에서 bfs를 하면서 우선순위를 찾아 기록
-궁수를 3가지 위치에 배정
-
-이미 제거된 적을 담을 셋
-이번에 제거할 적을 담을 셋
-
-우선순위순서로 찾고 이미 제거된 적이 아닐 경우 해당인덱스 기록 & 이번에 제거할 적을 담을 셋에 기록
-
-1. 궁수 3 배치하는 방법들.
-
-'''
-def bfs():
-    for i in range(M):
-        vl = [[False for _ in range(N)] for _ in range(M)]
-        vl[N-1][i] = True
-        Q.append([i, N-1, -D])
-        if nl[N-1][i]:
-            el[i].append([nl[N-1][i], 1])
-
-
-        xl, xh = max(0, i-1), min(M, i+2)
-        while Q:
-            x, y, dis = Q.popleft()
-            for dx, dy in d:
-                tx, ty = x + dx, y + dy
-                if xl <= tx < xh and 0 <= ty < N and not vl[ty][tx]:
-                    vl[ty][tx] = True
-                    Q.append([tx, ty, dis+1])
-                    if nl[ty][tx]:
-                        el[i].append([nl[ty][tx], dis+1])
-            if len(el[i]) == cnt+1:
-                Q.clear()
-                break
-
-
-def comb(c = 3, arr = [], dep = 0):
-    global r
-    if r == cnt:
-        return
-    if not c:
-        solve(arr)
-        return
-    for i in range(dep, N-c+1):
-        arr.append(i)
-        comb(c-1, arr, i+1)
-        arr.pop()
-
-
-def solve(arr):
-    global r
-    defeated = set()
-    time = 0
-    ac = 0
-    while 1:
-        time += 1
-        temp_defeated = set()
-        for i in range(3):
-            for j in range(len(el[i])):
-                if el[arr[i]][j][0] not in defeated and el[arr[i]][j][1] <= time:
-                    temp_defeated.add(el[arr[i]][j][0])
-                    break
-        ac += len(temp_defeated)
-        defeated = defeated.union(temp_defeated)
-        for i, n in enumerate(el2):
-            if n == time:
-                defeated.add(i)
-        if len(defeated) == cnt:
-            r = max(r, ac)
-            return
+def bfs(i, j):
+    Q = deque([[i, j, 0]])
+    vl = [[0 for _ in range(M)] for _ in range(N)]
+    vl[j][i] = 1
+    while Q:
+        x, y, d = Q.popleft()
+        if d == D:
+            return -1, -1
+        if (x, y) in tes:
+            return x, y
+        for dx, dy in Di:
+            tx, ty = x + dx, y + dy
+            if 0 <= tx < M and 0 <= ty < N and not vl[ty][tx]:
+                vl[ty][tx] = 1
+                Q.append([tx, ty, d+1])
+    return -1, -1
 
 
 
-
-d = [[-1, 0], [0, -1], [1, 0]]
-N, M, D = map(int, sys.stdin.readline().split())
-Q = deque()
-el = [[] for _ in range(M)]
-nl = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-
-
-# 적에게 번호 매겨 주기.
-cnt = 0
-el2 = []
+Di = [[-1, 0], [0, -1], [1, 0]]
+N, M, D = map(int, input().split())
+nl = [list(map(int, input().split())) for _ in range(N)]
+es = set()
 for j in range(N):
     for i in range(M):
         if nl[j][i]:
-            cnt += 1
-            nl[j][i] = cnt
-            el2.append(N-j)
-if not cnt:
-    print(0)
-else:
-    bfs()
-    r = 0
-    comb()
-    print(r)
+            es.add((i, j))
+r = 0
+for i in range(M-2):
+    for j in range(i+1, M-1):
+        for k in range(j+1, M):
+            tes = set()
+            for data in es:
+                tes.add(data)
+            cnt = 0
+            while tes:
+                t1, t2 = bfs(i, N-1)
+                t3, t4 = bfs(j, N-1)
+                t5, t6 = bfs(k, N-1)
+                if t1 != -1:
+                    if (t1, t2) in tes:
+                        tes.remove((t1, t2))
+                        cnt += 1
+                if t3 != -1:
+                    if (t3, t4) in tes:
+                        tes.remove((t3, t4))
+                        cnt += 1
+                if t5 != -1:
+                    if (t5, t6) in tes:
+                        tes.remove((t5, t6))
+                        cnt += 1
+                tes2 = set()
+                for x, y in tes:
+                    if y != N:
+                        tes2.add((x, y+1))
+                tes = tes2
+            r = max(cnt, r)
+print(r)
--- algorithm/study/test.py
+++ algorithm/study/test.py
@@ -1,1 +1,59 @@
-a
+from collections import deque
+def bfs(i, j):
+    Q = deque([[i, j, 0]])
+    vl = [[0 for _ in range(M)] for _ in range(N)]
+    vl[j][i] = 1
+    while Q:
+        x, y, d = Q.popleft()
+        if d == D:
+            return -1, -1
+        if (x, y) in tes:
+            return x, y
+        for dx, dy in Di:
+            tx, ty = x + dx, y + dy
+            if 0 <= tx < M and 0 <= ty < N and not vl[ty][tx]:
+                vl[ty][tx] = 1
+                Q.append([tx, ty, d+1])
+    return -1, -1
+
+
+
+Di = [[-1, 0], [0, -1], [1, 0]]
+N, M, D = map(int, input().split())
+nl = [list(map(int, input().split())) for _ in range(N)]
+es = set()
+for j in range(N):
+    for i in range(M):
+        if nl[j][i]:
+            es.add((i, j))
+r = 0
+for i in range(M-2):
+    for j in range(i+1, M-1):
+        for k in range(j+1, M):
+            tes = set()
+            for data in es:
+                tes.add(data)
+            cnt = 0
+            while tes:
+                t1, t2 = bfs(i, N-1)
+                t3, t4 = bfs(j, N-1)
+                t5, t6 = bfs(k, N-1)
+                if t1 != -1:
+                    if (t1, t2) in tes:
+                        tes.remove((t1, t2))
+                        cnt += 1
+                if t3 != -1:
+                    if (t3, t4) in tes:
+                        tes.remove((t3, t4))
+                        cnt += 1
+                if t5 != -1:
+                    if (t5, t6) in tes:
+                        tes.remove((t5, t6))
+                        cnt += 1
+                tes2 = set()
+                for x, y in tes:
+                    if y != N:
+                        tes2.add((x, y+1))
+                tes = tes2
+            r = max(cnt, r)
+print(r)
