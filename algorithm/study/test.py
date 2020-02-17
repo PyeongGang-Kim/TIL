e1d1aa893bb37be55d4772e8a9f2ff5aa7f939b6
PyeongGang-Kim <pyeonggangkim@gmail.com>
2020-02-18 00:07:56 +0900

    02/18

--- algorithm/study/test.py
+++ algorithm/study/test.py
@@ -1,26 +1,93 @@
-'''
-거쳐가는 경로가 있을 경우 현재 경로룰 없애도 된다(무한대)
-거쳐가는 경로가 더 작은경우 말이 안되는 경우이다(-1)
-그 외의 경우는 꼭 필요한 도로이다.
-'''
-def floid():
-    for k in range(N):
-        for i in range(N):
-            for j in range(N):
-                if i != j and i != k and k != j:
-                    if nl[i][j] != inf and nl[i][j] > nl[i][k] + nl[k][j]:
-                        return -1
-                    elif nl[i][j] == nl[i][k] + nl[k][j]:
-                            nl[i][j] = inf
-    r = 0
-    for j in range(N):
-        for i in range(j+1, N):
-            if nl[j][i] != inf:
-                r += nl[j][i]
-    return r
+from collections import deque
+import sys
+input = sys.stdin.readline
 
+def printroute():
+    if nl[n] != inf:
+        ml2 = [[] for _ in range(n + 1)]
+        for u in range(1, n+1):
+            for v, w in ml[u]:
+                ml2[v].append([u, w])
+        pos = n
+        r = []
+        vl = [0] * (n+1)
+        vl[n] = 1
+        while 1:
+            tmp = pos
+            for u, w in ml2[pos]:
+                if not vl[u] and w + nl[u] == nl[pos]:
+                    r.append(str(pos))
+                    pos = u
+                    vl[u] = True
+                    break
+            if pos == 1:
+                r.append('1')
+                break
+            if tmp == pos:
+                print(-1)
+                return
+        print(' '.join(reversed(r)))
+    else:
+        print(-1)
+
+
+def check():
+    ml2 = [[] for _ in range(n + 1)]
+    for u in range(1, n+1):
+        for v, w in ml[u]:
+            ml2[u].append([v, w])
+    while Q:
+        pos = Q.popleft()
+        if pos == n:
+            return True
+        vl[pos] = 1
+        for v, w in ml2[pos]:
+            if not vl[v]:
+                vl[v] = 1
+                Q.append(v)
+    return False
 
 inf = 0xfffffff
-N = int(input())
-nl = [list(map(int, input().split())) for _ in range(N)]
-print(floid())
+n, m = map(int, input().split())
+nl = [inf for _ in range(n+1)]
+ml = [[] for _ in range(n + 1)]
+while m:
+    m -= 1
+    u, v, w = map(int, input().split())
+    ml[u].append([v, w])
+
+i = n - 1
+nl[1] = 0
+while i:
+    i -= 1
+    update = False
+    for u in range(1, n):
+        for v, w in ml[u]:
+            if nl[u] != inf:
+                if nl[v] != inf:
+                    if nl[u] + w > nl[v]:
+                        nl[v] = nl[u] + w
+                        update = True
+                else:
+                    nl[v] = nl[u] + w
+    if not update:
+        break
+
+Q = deque()
+
+vl = [0] * (n + 1)
+if update:
+    for i in range(1, n):
+        for v, w in ml[u]:
+            if nl[u] != inf:
+                if nl[u] + w > nl[v]:
+                    Q.append(u)
+                    vl[u] = 1
+
+    if check():
+        print(-1)
+    else:
+        printroute()
+else:
+    printroute()
+
