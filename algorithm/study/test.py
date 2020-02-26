0822c196769bd54100d860669231059859c82b15
PyeongGang-Kim <pyeonggangkim@gmail.com>
2020-02-26 20:50:50 +0900

    02/26

--- algorithm/study/test.py
+++ algorithm/study/test.py
@@ -1,32 +1,46 @@
 import sys
 import heapq
-import math
 input = sys.stdin.readline
 
+inf = 2500
+D = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
-n = int(input())
-nl = [list(map(float, input().split())) for _ in range(n)]
-ml = [[] for _ in range(n)]
-for j in range(n):
-    for i in range(n):
-        if j != i:
-            ml[j].append([(nl[i][0] - nl[j][0])**2 + (nl[i][1] - nl[j][1])**2, j, i])
-s = 0
-Q = []
-for item in ml[0]:
-    heapq.heappush(Q, item)
-vl = [0] * n
-vl[0] = 1
-cnt = n-1
-while Q:
-    dis, idx1, idx2 = heapq.heappop(Q)
-    if not vl[idx2]:
-        s += math.sqrt(dis)
-        for item in ml[idx2]:
-            heapq.heappush(Q, item)
-        vl[idx2] = 1
-        cnt -= 1
-    if not cnt:
-        break
 
-print(s)
+def bfs(x, y):
+    global M
+    Q = [[0, x, y]]
+    while Q:
+        dis, x, y = heapq.heappop(Q)
+        for dx, dy in D:
+            tx, ty = x + dx, y + dy
+            if 0 <= tx < N and 0 <= ty < N and dis + 1 < vl[ty][tx] and ml[ty][tx] != '1':
+                if ml[ty][tx] == 'K':
+                    if vl[ty][tx] == inf:
+                        vl[ty][tx] = dis + 1
+                        heapq.heappush(Q, [0, tx, ty])
+                else:
+                    vl[ty][tx] = dis + 1
+                    heapq.heappush(Q, [dis+1, tx, ty])
+    s = 0
+    for j in range(N):
+        for i in range(N):
+            if ml[j][i] == 'K':
+                if vl[j][i] == inf:
+                    print(-1)
+                    return
+                s += vl[j][i]
+    print(s)
+
+
+def init():
+    for j in range(N):
+        for i in range(N):
+            if ml[j][i] == 'S':
+                bfs(i, j)
+                return
+
+
+N, M = map(int, input().split())
+ml = [input() for _ in range(N)]
+vl = [[inf for _ in range(N)] for _ in range(N)]
+init()
