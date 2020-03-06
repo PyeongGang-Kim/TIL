28305655591e8f4dcd2ca1a8c4e91b1cc7f8fc3e
PyeongGang-Kim <pyeonggangkim@gmail.com>
2020-03-06 14:07:53 +0900

    03/06

--- algorithm/swex/3752.py
+++ algorithm/swex/3752.py
@@ -1,13 +1,16 @@
-r = []
 T = int(input())
-for t in range(1, T+1):
+for tc in range(1, T+1):
     N = int(input())
     nl = list(map(int, input().split()))
-    tmp = {0}
-    for i in range(N):
-        tmp1 = set()
-        for j in tmp:
-            tmp1.add(j+nl[i])
-        tmp = tmp.union(tmp1)
-    r.append('#{} {}'.format(t, len(tmp)))
-print('\n'.join(r))
+    cl = [0] * (sum(nl)+1)
+    tmp = [0]
+    cl[0] = 1
+    cnt = 1
+    for k in range(N):
+        for i in range(len(tmp)):
+            tmp2 = tmp[i] + nl[k]
+            if not cl[tmp2]:
+                cl[tmp2] = 1
+                tmp.append(tmp2)
+                cnt += 1
+    print('#{} {}'.format(tc, cnt))
