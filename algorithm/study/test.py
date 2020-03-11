9936f295f5e567fe051c319cb7484c99aa68af54
PyeongGang-Kim <pyeonggangkim@gmail.com>
2020-03-11 14:48:01 +0900

    03/11

--- algorithm/study/test.py
+++ algorithm/study/test.py
@@ -1,17 +1,30 @@
 
-n = int(input())
 
-nl = [0] * (n+1)
-
-
-for i in range(n):
-    k = str(i)
-    tmp = i
-    for s in k:
-        tmp += int(s)
-
-    if tmp <= n:
-        if not nl[tmp]:
-            nl[tmp] = i
-
-print(nl[n] if nl[n] else 0)
+numbers = []
+opers = []
+tmp = ''
+for s in '1T2D3D#':
+    if s.isnumeric():
+        tmp += s
+    else:
+        if tmp:
+            numbers.append(int(tmp))
+            tmp = ''
+        opers.append(s)
+print(numbers)
+print(opers)
+idx = -1
+for oper in opers:
+    if oper in 'SDT':
+        idx += 1
+        if oper == 'D':
+            numbers[idx] **= 2
+        elif oper == 'T':
+            numbers[idx] **= 3
+    if oper == '*':
+        numbers[idx] *= 2
+        if idx:
+            numbers[idx-1] *= 2
+    if oper == '#':
+        numbers[idx] *= -1
+print(sum(numbers))
