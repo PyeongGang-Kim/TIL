5b8b9b722a1e55418e2d055b5379447916b5a409
PyeongGang-Kim <pyeonggangkim@gmail.com>
2020-03-31 09:18:27 +0900

    03/31

--- algorithm/백준/test.py
+++ algorithm/백준/test.py
@@ -1,48 +1,71 @@
-def findstart():
-    for j in range(M):
-        for i in range(N):
-            if nl[j][i] == 2:
-                return i, j
+import sys
 
 
-def dfs(x, y, dep = 0, pos = 0):
-    if vl2[pos][y][x]<= dep:
-        return
-    vl2[pos][y][x] = dep
-    if nl[y][x] == 3:
-        if not pos:
-            global cnt
-            cnt = min(cnt, dep)
-        return
-    for i in range(4):
-        tx, ty = x + D[i][0], y + D[i][1]
-        if 0<=tx<N and 0<=ty<M and nl[ty][tx] and not vl[ty][tx]:
-            vl[ty][tx] = 1
-            dfs(tx, ty, dep+1, tr[pos][i])
-            vl[ty][tx] = 0
+# import heapq
+# input = sys.stdin.readline
+# sys.stdin = open('input.txt', encoding='utf8')
+
+def ria():
+    return list(map(int, input().split()))
 
 
-tr = [
-    [3, 4, 1, 2],
-    [0, 1, 5, 1],
-    [2, 0, 2, 5],
-    [5, 3, 0, 3],
-    [4, 5, 4, 0],
-    [1, 2, 3, 4],
-]
-D = [[0, -1], [-1, 0], [0, 1], [1, 0]]
-inf = 0xfffffff
-T = int(input())
-for tc in range(1, T+1):
-    N, M = map(int, input().split())
-    nl = [list(map(int, input().split())) for _ in range(M)]
-    vl = [[0 for _ in range(N)] for _ in range(M)]
-    vl2 = [[[inf for _ in range(N)] for _ in range(M)] for _ in range(6)]
-    S = findstart()
-    vl[S[1]][S[0]] = 1
-    cnt = 0xfffffff
-    dfs(*S)
-    if cnt!=0xfffffff:
-        print('#{} {}'.format(tc, cnt))
-    else:
-        print('#{} {}'.format(tc, -1))
+sys.setrecursionlimit(100000)
+
+
+def solve():
+    v, e = ria()
+    # print(v,e)
+
+    visited = [False] * (v + 10)
+    finished = [False] * (v + 10)
+    adj = [[] for x in range(e + 10)]
+    stack = []
+    curScc = []
+    SCC = []
+    dfsn = [0] * (v + 10)
+    curn = 1
+
+    for i in range(e):
+        a, b = ria()
+        adj[a].append(b)
+
+    def dfs(cur):
+        nonlocal stack, curScc, SCC, dfsn, curn, adj, visited, finished
+
+        stack.append(cur)
+
+        dfsn[cur] = curn
+        curn += 1
+
+        result = dfsn[cur]
+
+        for there in adj[cur]:
+            if (dfsn[there] == 0):
+                result = min(result, dfs(there))
+            elif (not finished[there]):
+                result = min(result, dfsn[there])
+
+        if (result == dfsn[cur]):
+            curScc = []
+            while (True):
+                tt = stack.pop()
+                curScc.append(tt)
+                finished[tt] = True
+                if (tt == cur): break
+            curScc.sort()
+            SCC.append(curScc)
+
+        return result
+
+    for i in range(1, v + 1):
+        if (dfsn[i] == 0): dfs(i)
+
+    # print(adj)  
+    # print(dfsn)
+    SCC.sort()
+    # print(SCC)
+
+    print(len(SCC))
+    for i, sc in enumerate(SCC):
+        print(' '.join(map(str, sc)) + ' -1')
+solve()
